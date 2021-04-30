4ead11680f61f8efc1efc5e417267b9fe98748d2
Emine Cheikh <cheikhemine@gmail.com>
2021-04-30 16:25:55 +0000

    added function roundOne and outputs

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-function computerPlay () {
+let computerPlay = function () {
     let selections = ["rock", "paper", "scissor"];
     let selection = selections[Math.floor(Math.random()*selections.length)];
-   return selection
+ return selection
 }
 
+
+
+
+let roundOne = function (playerSelection, computerSelection) {
+if (playerSelection === computerSelection) {
+return "It's tie you bitches!"
+}
+   else if (playerSelection === "rock" && computerSelection === "scissor") {
+       return "You win bitch!"
+   }  else if (computerSelection === "rock" && playerSelection === "scissor") {
+       return "You lose bitch! rock beat scissor"
+   } else if (playerSelection === "scissor" && computerSelection === "paper") {
+       return "you win bitch! scissor beat paper"
+   } else if (computerSelection === "scissor" && playerSelection === "paper") {
+       return "you lose amigo! scissor beat paper"
+   } else if (playerSelection === "paper" && computerSelection === "rock") {
+       return "you win bitch! paper cover the rock"
+   } else if (computerSelection === "paper" && playerSelection === "rock") {
+       return "you lose fucker! paper beat rock"
+   }
+}
+
+
+
+
+const playerSelection = prompt("choose wisely")
+computerSelection = computerPlay()
+
+console.log(roundOne(playerSelection, computerSelection));
