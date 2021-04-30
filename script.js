58666d207990c4510ceec8c2280538d12318d227
Emine Cheikh <cheikhemine@gmail.com>
2021-04-30 21:29:16 +0000

    modified roundOne function to inlude score

--- script.js
+++ script.js
@@ -5,26 +5,39 @@ let computerPlay = function () {
 }
 
 
+let userScore = 0;
+let computerScore = 0;
 
 
 let roundOne = function (playerSelection, computerSelection) {
 if (playerSelection === computerSelection) {
-return "It's tie!"
-}
-   else if (playerSelection === "rock" && computerSelection === "scissor") {
-       return "You win!"
+alert("It's a tie!!")
+}else if (playerSelection === "rock" && computerSelection === "scissor") {
+       alert("You win! rock beat scissor!");
+       userScore++;
    }  else if (computerSelection === "rock" && playerSelection === "scissor") {
-       return "You lose! rock beat scissor"
+     alert("You lose! rock beat scissor");
+    computerScore++;
    } else if (playerSelection === "scissor" && computerSelection === "paper") {
-       return "you win!r"
+  alert("you win! scissor beat paper");
+  userScore++;
    } else if (computerSelection === "scissor" && playerSelection === "paper") {
-       return "you lose! scissor beat paper"
-   } else if (playerSelection === "paper" && computerSelection === "rock") {
-       return "you win!"
+   alert("you lose! scissor beat paper");
+  computerScore++;
+   } else if (playerSelection === "paper" && playerSelection === "rock") {
+    alert("You win! Paper beat rock") 
+   userScore++;
    } else if (computerSelection === "paper" && playerSelection === "rock") {
-       return "you lose! paper beat rock"
+       alert ("You lose! Paper beat rock")
+       computerScore++;
+   } else {
+       alert("incorrect reply")
    }
+   console.log(userScore, computerScore)
+   
 }
+
+
 
 
 
@@ -33,3 +46,6 @@ const playerSelection = prompt("choose rock, paper or scissor")
 computerSelection = computerPlay()
 
 console.log(roundOne(playerSelection, computerSelection));
+
+
+
